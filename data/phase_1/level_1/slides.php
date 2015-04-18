08cbc0f32b1bebd0e8c0f3b7f0280567e8be72a4
Paul Corpuz <paul.corpuz@gmail.com>
2015-04-18 12:55:12 +0800

    Added game beta release

--- data/phase_1/level_1/slides.php
+++ data/phase_1/level_1/slides.php
@@ -3,7 +3,17 @@ class Slides {
 
   public function Slide_1() {
     $text = <<<EOF
-(DIALOGUE GOES HERE)
+(SLIDE 1 DIALOGUE GOES HERE)
+EOF;
+    return array(
+      'speaker' => 'player',
+      'dialogue' => $text,
+    );
+  }
+
+  public function Slide_2() {
+    $text = <<<EOF
+(SLIDE 2 DIALOGUE GOES HERE)
 EOF;
     return array(
       'speaker' => 'player',
